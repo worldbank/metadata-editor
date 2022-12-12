84b102696b7d5b0eed3e317aeb5b312f91ee6fc1
Mehmood Asghar <mah0001@gmail.com>
2022-12-12 13:47:49 -0500

    Editor configuration file

---
+++ application/config/editor.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Configurations for Metadata editor
+|--------------------------------------------------------------------------
+|
+|
+*/
+
+//Storage root folder for editor
+$config['editor']['storage_path']='datafiles/editor';
+
+//R OpenCPU API base URL
+$config['editor']['r_api_url']='http://localhost:2121/ocpu/library/';
+
+//Folder for R to expore/write files
+$config['editor']['r_storage_path']='datafiles/r';
