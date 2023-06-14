4d2c652156ecac13a38ab68fc05422db77eb4e49
Mehmood Asghar <mah0001@gmail.com>
2023-06-14 17:22:32 -0400

    minor

--- application/config/template.php
+++ application/config/template.php
@@ -79,8 +79,8 @@ $template['base_url']=defined('JS_BASE_URL') ? JS_BASE_URL : base_url();
 |--------------------------------------------------------------------------
 */
 //installer template file with only the body and no layout
-$template['installer']['template'] = '../../themes/nada/installer';
-$template['installer']['theme_folder'] = 'nada';
+$template['installer']['template'] = '../../themes/nada52/installer';
+$template['installer']['theme_folder'] = 'nada52';
 
 
 //blank template file with only the body and no layout
