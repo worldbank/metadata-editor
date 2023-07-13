a59b290605454154d02652cfb21f2f597f72353a
Mehmood Asghar <mah0001@gmail.com>
2023-07-13 09:12:43 +0200

    minor

--- application/config/config.php
+++ application/config/config.php
@@ -656,7 +656,7 @@ $config['site_user_register']='yes';
 |
 | allowed_resource_types	comma seperated list of file extensions that are allowed for uploading external resources
 */
-$config['allowed_resource_types']='jpg,gif,png,zip,doc,docx,pdf,sav,dta,txt,xls,xlsx,ppt,csv,rar,do,r';
+$config['allowed_resource_types']='jpg,gif,png,zip,doc,docx,pdf,sav,dta,txt,xls,xlsx,ppt,csv,rar,do,r,pptx';
 
 
 /*
