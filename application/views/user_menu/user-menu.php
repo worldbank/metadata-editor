904d40cf08898ae28a3972d9e6d96ac943f4ef43
Mehmood Asghar <mah0001@gmail.com>
2024-04-15 10:41:50 -0400

    minor

--- application/views/user_menu/user-menu.php
+++ application/views/user_menu/user-menu.php
@@ -13,7 +13,7 @@ $user=$this->session->userdata('username');
     <?php if ($user!=''): ?>
         <div class="dropdown ml-auto">
             <a class="nav-link dropdown-toggle" style="min-width:70px;" href="#" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-user-circle fa-lg"></i>  <span class="text-capitalize"><?php echo username_initials($user); ?></span>
+                <i class="fas fa-user-circle fa-lg"></i>  <span class="text-capitalize"><?php echo ($user); ?></span>
             </a>
 
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">                
