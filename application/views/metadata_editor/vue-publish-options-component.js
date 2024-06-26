846eec144772f720078f1c6ef9bbde42aa3e3803
Mehmood Asghar <mah0001@gmail.com>
2024-06-25 20:56:44 -0400

    fix formatting

--- application/views/metadata_editor/vue-publish-options-component.js
+++ application/views/metadata_editor/vue-publish-options-component.js
@@ -471,136 +471,133 @@ Vue.component('publish-options', {
                 
                     <v-card-text>
                     
-                <v-card 
-                    elevation="2" class="p-3 mb-3"
-                >
-                        <div class="form-group" elevation="10">
-                            <label for="catalog_id">{{$t("catalog")}} <router-link class="btn btn-sm btn-link" to="/configure-catalog">{{$t("configure_catalog")}}</router-link></label>
-                            <select class="form-control" id="catalog_id" v-model="catalog" @change="onCatalogSelection">
-                                <option :value="false">-Select-</option>
-                                <option v-for="(option,index) in catalog_connections" v-bind:value="index">
-                                    {{ option.title }} - {{option.url}}
-                                </option>                            
-                            </select>
-                            
-                            <div v-if="catalog" class="text-muted">{{catalog_connections[catalog]}}</div>                            
-                        </div>
-
-                </v-card>
-
-                <v-expansion-panels multiple v-model="panels">
-                    <v-expansion-panel>
-                        <v-expansion-panel-header>
-                            {{$t("project_options")}}
-                        </v-expansion-panel-header>
-                        <v-expansion-panel-content>
-
-                        <div class="mb-4">
-                            
-                            <table class="table table-sm table-bordered table-hover table-striped mb-0 pb-0" style="font-size:small;">
-                                <tr>
-                                    <th>{{$t("option")}}</th>
-                                    <th>{{$t("value")}}</th>
-                                </tr>
-                                <template v-for="(kv,kv_key) in publish_options">                                            
-                                <tr v-if="!kv.custom">
-                                    <td>
-                                        {{kv.title}}                                        
-                                    </td>
-                                    <td>
-                                        <input v-if="!kv.enum" type="text" class="form-control" v-model="kv.value"/>
-                                        <select v-if="kv.enum" class="form-control" v-model="kv.value">
-                                            <option v-for="(enum_val,enum_key) in kv.enum" v-bind:value="enum_key">
-                                                {{ enum_val }}
-                                            </option>
-                                        </select>
-                                    </td>
-                                </tr>                                            
-                                </template>
-                                <tr>
-                                    <td>{{$t("data_access")}} <v-icon @click="onCatalogSelection">mdi-reload</v-icon></td>
-                                    <td>
-                                        <select v-if="data_access_list" class="form-control" v-model="publish_options.access_policy.value">
-                                            <option value="">N/A</option>
-                                            <option v-for="(data_access,da_index) in data_access_list" v-bind:value="data_access.type">
-                                                {{ data_access.title }} - [{{ data_access.type }}]
-                                            </option>
-                                        </select>
-
-                                        <div v-if="publish_options.access_policy.value=='remote'" class="p-2">
-                                            <label>Link to remote repository</label>
-                                            <input class="form-control" type="text" v-model="publish_options.data_remote_url.value">
-                                        </div>
-
-                                    </td>
-
-                                </tr>
-                                <tr>
-                                    <td>{{$t("collection")}} <v-icon @click="onCatalogSelection">mdi-reload</v-icon></td>
-                                    <td>
-                                        <select v-if="collections" class="form-control" v-model="publish_options.repositoryid.value">
-                                            <option value="">N/A</option>
-                                            <option v-for="(collection,collection_index) in collections" v-bind:value="collection.repositoryid">
-                                                {{ collection.title }} - [{{ collection.repositoryid }}]
-                                            </option>
-                                        </select>                                    
-                                    </td>
-
-                                </tr>
+                    <v-card elevation="2" class="p-3 mb-3">
+                            <div class="form-group" elevation="10">
+                                <label for="catalog_id">{{$t("catalog")}} <router-link class="btn btn-sm btn-link" to="/configure-catalog">{{$t("configure_catalog")}}</router-link></label>
+                                <select class="form-control" id="catalog_id" v-model="catalog" @change="onCatalogSelection">
+                                    <option :value="false">-Select-</option>
+                                    <option v-for="(option,index) in catalog_connections" v-bind:value="index">
+                                        {{ option.title }} - {{option.url}}
+                                    </option>                            
+                                </select>
                                 
-                            </table>                            
-                        </div>
-                        </v-expansion-panel-content>
-                    </v-expansion-panel>
+                                <div v-if="catalog" class="text-muted">{{catalog_connections[catalog]}}</div>                            
+                            </div>
+                    </v-card>
 
-                    <v-expansion-panel>
-                        <v-expansion-panel-header>
-                            <div>{{$t("external_resources")}}
-                                <div class="text-secondary text-muted text-xs text-small text-normal">{{$t("select_external_resources_to_be_published")}}</div>
-                            </div>
-                            
-                        </v-expansion-panel-header>
-                        <v-expansion-panel-content>
-                            <div class="mt-3">
-                                    <v-switch
-                                    v-model="resources_overwrite"
-                                    value="yes"
-                                    :label="$t('overwrite_resources')"
-                                ></v-switch>
-                            </div>
-                            
-                            <div v-if="ExternalResources.length>0" >
-                                <div>
-                                    <strong>{{ExternalResources.length}}</strong> {{$t("n_resources_found")}}
-                                    <span class="ml-2"><strong>{{resources_selected.length}}</strong> {{$t("n_selected")}}</span>
-                                </div>
-                                <div class="border" style="max-height:300px;overflow:auto;">                    
-                                    <table class="table table-sm table-striped">
-                                        <thead>
-                                        <tr class="bg-light">
-                                            <th><input type="checkbox" v-model="toggle_resources_selected" @change="toggleSelectedResources"></th>
-                                            <th>{{$t("title")}}</th>
-                                            <th>{{$t("type")}}</th>
-                                        </tr>
-                                        </thead>
-                                        <tr v-for="(resource,resource_index) in ExternalResources" :key="resource.id">
-                                            <td><input type="checkbox" :value="resource_index" v-model="resources_selected"></td>
-                                            <td>
-                                                <div>{{resource.title}}</div>
-                                                <div class="text-secondary text-small">{{resource.filename}}</div>
-                                            </td>
-                                            <td>{{resource.dctype}}</td>
-                                        </tr>
-                                    </table>
-                                </div>
-                            </div>
-                            <div v-else class="alert alert-warning">
-                            {{$t("no_external_resources_found")}}
-                            </div>
-                        </v-expansion-panel-content>
-                    </v-expansion-panel>
+                    <v-expansion-panels multiple v-model="panels">
+                        <v-expansion-panel>
+                            <v-expansion-panel-header>
+                                {{$t("project_options")}}
+                            </v-expansion-panel-header>
+                            <v-expansion-panel-content>
 
-                </v-expansion-panels>
+                            <div class="mb-4">
+                                
+                                <table class="table table-sm table-bordered table-hover table-striped mb-0 pb-0" style="font-size:small;">
+                                    <tr>
+                                        <th>{{$t("option")}}</th>
+                                        <th>{{$t("value")}}</th>
+                                    </tr>
+                                    <template v-for="(kv,kv_key) in publish_options">                                            
+                                    <tr v-if="!kv.custom">
+                                        <td>
+                                            {{kv.title}}                                        
+                                        </td>
+                                        <td>
+                                            <input v-if="!kv.enum" type="text" class="form-control" v-model="kv.value"/>
+                                            <select v-if="kv.enum" class="form-control" v-model="kv.value">
+                                                <option v-for="(enum_val,enum_key) in kv.enum" v-bind:value="enum_key">
+                                                    {{ enum_val }}
+                                                </option>
+                                            </select>
+                                        </td>
+                                    </tr>                                            
+                                    </template>
+                                    <tr>
+                                        <td>{{$t("data_access")}} <v-icon @click="onCatalogSelection">mdi-reload</v-icon></td>
+                                        <td>
+                                            <select v-if="data_access_list" class="form-control" v-model="publish_options.access_policy.value">
+                                                <option value="">N/A</option>
+                                                <option v-for="(data_access,da_index) in data_access_list" v-bind:value="data_access.type">
+                                                    {{ data_access.title }} - [{{ data_access.type }}]
+                                                </option>
+                                            </select>
+
+                                            <div v-if="publish_options.access_policy.value=='remote'" class="p-2">
+                                                <label>Link to remote repository</label>
+                                                <input class="form-control" type="text" v-model="publish_options.data_remote_url.value">
+                                            </div>
+
+                                        </td>
+
+                                    </tr>
+                                    <tr>
+                                        <td>{{$t("collection")}} <v-icon @click="onCatalogSelection">mdi-reload</v-icon></td>
+                                        <td>
+                                            <select v-if="collections" class="form-control" v-model="publish_options.repositoryid.value">
+                                                <option value="">N/A</option>
+                                                <option v-for="(collection,collection_index) in collections" v-bind:value="collection.repositoryid">
+                                                    {{ collection.title }} - [{{ collection.repositoryid }}]
+                                                </option>
+                                            </select>                                    
+                                        </td>
+
+                                    </tr>
+                                    
+                                </table>                            
+                            </div>
+                            </v-expansion-panel-content>
+                        </v-expansion-panel>
+
+                        <v-expansion-panel>
+                            <v-expansion-panel-header>
+                                <div>{{$t("external_resources")}}
+                                    <div class="text-secondary text-muted text-xs text-small text-normal">{{$t("select_external_resources_to_be_published")}}</div>
+                                </div>
+                                
+                            </v-expansion-panel-header>
+                            <v-expansion-panel-content>
+                                <div class="mt-3">
+                                        <v-switch
+                                        v-model="resources_overwrite"
+                                        value="yes"
+                                        :label="$t('overwrite_resources')"
+                                    ></v-switch>
+                                </div>
+                                
+                                <div v-if="ExternalResources.length>0" >
+                                    <div>
+                                        <strong>{{ExternalResources.length}}</strong> {{$t("n_resources_found")}}
+                                        <span class="ml-2"><strong>{{resources_selected.length}}</strong> {{$t("n_selected")}}</span>
+                                    </div>
+                                    <div class="border" style="max-height:300px;overflow:auto;">                    
+                                        <table class="table table-sm table-striped">
+                                            <thead>
+                                            <tr class="bg-light">
+                                                <th><input type="checkbox" v-model="toggle_resources_selected" @change="toggleSelectedResources"></th>
+                                                <th>{{$t("title")}}</th>
+                                                <th>{{$t("type")}}</th>
+                                            </tr>
+                                            </thead>
+                                            <tr v-for="(resource,resource_index) in ExternalResources" :key="resource.id">
+                                                <td><input type="checkbox" :value="resource_index" v-model="resources_selected"></td>
+                                                <td>
+                                                    <div>{{resource.title}}</div>
+                                                    <div class="text-secondary text-small">{{resource.filename}}</div>
+                                                </td>
+                                                <td>{{resource.dctype}}</td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>
+                                <div v-else class="alert alert-warning">
+                                {{$t("no_external_resources_found")}}
+                                </div>
+                            </v-expansion-panel-content>
+                        </v-expansion-panel>
+
+                    </v-expansion-panels>
 
                                             
                     <div class=" mb-3 mt-5 switch-control">
@@ -622,12 +619,11 @@ Vue.component('publish-options', {
                                 v-model="publish_resources"
                                 :value="true"
                                 :label="$t('external_resources') + (resources_selected.length>0?' ('+resources_selected.length+')':'')"
-                            ></v-switch>
-                           
+                            ></v-switch>                            
                     </div>
 
                     <v-btn :disabled="is_publishing==true" color="primary" @click="publishToCatalog()">{{$t("publish")}}</v-btn>
-                </div>
+                
 
                 </v-card-text>
                 </v-card>
