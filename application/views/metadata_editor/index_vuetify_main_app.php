cfa6b153f35ca7d963e6c750e1d99fdbfcceb145
Mehmood Asghar <mah0001@gmail.com>
2024-08-01 11:40:04 -0400

    Update URL validation function - is_uri

--- application/views/metadata_editor/index_vuetify_main_app.php
+++ application/views/metadata_editor/index_vuetify_main_app.php
@@ -762,13 +762,13 @@
 
         VeeValidate.extend('is_uri', {
             validate(value){
-                var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-                        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-                        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-                        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-                        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-                        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-                return !!pattern.test(value);
+                
+                try { 
+                    return Boolean(new URL(value)); 
+                }
+                catch(e){ 
+                    return false; 
+                }
             },
             getMessage: (field, params, data) => {
                 return data.message;
